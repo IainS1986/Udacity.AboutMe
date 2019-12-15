9afc98a633281fef6a1d9d80224fc76abcb2a725
Iain <i_stanford@hotmail.com>
2019-12-15 16:09:18 +0000

    Lesson 15

--- app/src/main/java/com/stanford/udacity/aboutme/MainActivity.kt
+++ app/src/main/java/com/stanford/udacity/aboutme/MainActivity.kt
@@ -1,35 +1,35 @@
 package com.stanford.udacity.aboutme
 
 import android.content.Context
-import android.inputmethodservice.InputMethodService
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.stanford.udacity.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // Hook up done button
-        findViewById<Button>(R.id.doneButton).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(button: View) {
-        val editText = findViewById<EditText>(R.id.nicknameEdit)
-        val nicknameText = findViewById<TextView>(R.id.nicknameText)
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+            button.visibility = View.GONE
 
-        nicknameText.text = editText.text
-        editText.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
-        button.visibility = View.GONE
+            invalidateAll()
+        }
 
         // HideKeyboard (Util class?)
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
